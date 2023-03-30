3290a1eedd340c994e46d3e2c5885b2936bda43a
macsoftechnologies <macsoftechnologies@gmail.com>
2023-03-30 14:31:18 +0530

    navbar css changes and side menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
-import React from "react";
+// <<<<<<< HEAD
+import React, { useState } from "react";
 import Sidebar from './basecomp/Sidebar'
 import Navbar from './basecomp/Navbar'
 // import Sidebar from "./"
-=======
-import React, { useState } from "react";
-import Navbar from "./basecomp/Navbar";
 import { createContext } from "react";
 import ReactSwitch from "react-switch";
 import "./basecomp/App.css";
@@ -13,19 +10,15 @@ import "./basecomp/App.css";
 
 export const ThemeContext = createContext(null);
 
->>>>>>> 229181116d734019c914d90085584361fb1d6616
 function App() {
   const [theme, setTheme] = useState("dark");
   const toggleTheme = () => {
     setTheme((curr) => (curr === "light" ? "dark" : "light"));
   };
   return (
-<<<<<<< HEAD
     <>
     <Navbar/>
       <Sidebar/>
-    </>
-=======
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
       <div className="App" id={theme}>
         <Navbar />
@@ -35,7 +28,7 @@ function App() {
         {/* <Dropdown /> */}
       </div>
     </ThemeContext.Provider>
->>>>>>> 229181116d734019c914d90085584361fb1d6616
+    </>
   );
 }
 
