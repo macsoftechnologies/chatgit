647d0c22e0ceda6aa8d2091b57615dd4b46b3155
lohith146 <lohith.146@gmail.com>
2023-04-10 16:46:04 +0530

    signin api integration

---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ErrorPage() {
+  return (
+    <main className="text-center p-3">
+      <h1>An error occured!</h1>
+      <p>Could not find the page!</p>
+    </main>
+  );
+}
+
+export default ErrorPage;
